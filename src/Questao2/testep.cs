07e67fe3cf60cdb13244d4c9c7f9bf5f18d587dd
Sandra Fernandes <sandrapatfer@gmail.com>
2011-05-04 20:03:41 +0100

    Primeira entrega

---
+++ src/Questao2/testep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sessao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Ponto p1 = new Ponto(1,2), p2 = new Ponto(3,4);
+			Console.WriteLine(p1.ToString());
+			Console.WriteLine(p2.ToString());
+			Console.WriteLine("Distancia: {0}", p1.Distance(p2));
+        }
+    }
+}
